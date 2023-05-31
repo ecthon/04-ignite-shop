c4553a3c145d01d62622307844c768bf532c9010
newecthon <ecthon@gmail.com>
2023-05-31 10:27:49 -0300

    config getCssText

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+    config,
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme
+} = createStitches({
+    theme: {
+        colors: {
+            rocketseat: '#8257e6'
+        }
+    }
+})
