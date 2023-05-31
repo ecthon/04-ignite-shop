d9efd45f3c41a72412f19b5d747d876b1afdb0df
newecthon <ecthon@gmail.com>
2023-05-31 14:51:10 -0300

    Navigation - Link from next/link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 import { HomeContainer, Product } from '@/styles/pages/home'
 
+import { Link } from 'next/link'
+
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 interface HomeProps {
@@ -26,13 +28,15 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => (
-        <Product key={product.id} className='keen-slider__slide'>
-        <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
-        <footer>
-          <strong>{product.name}</strong>
-          <span>{product.price}</span>
-        </footer>
-      </Product>
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <Product className='keen-slider__slide'>
+            <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   )
