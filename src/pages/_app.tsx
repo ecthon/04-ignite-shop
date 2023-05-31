724ee20eeb69ad9bfb2f5cc9febbf6ce7f6b862e
newecthon <ecthon@gmail.com>
2023-05-31 11:14:13 -0300

    Estrutura visual - Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image';
+import { Container, Header } from '@/styles/pages/app';
+
 globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logo do Ignite"/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
