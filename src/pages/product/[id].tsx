f0f33ca67fd0b58feceaab502bd2ba5370b18d62
newecthon <ecthon@gmail.com>
2023-05-31 17:44:20 -0300

    feat: product and checkout - buy button

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
 import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 
 interface ProductProps {
@@ -12,10 +14,29 @@ interface ProductProps {
         imageUrl: string;
         price: string;
         description: string;
+        defaultPriceId: string;
     }
 }
 
 export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+    
+    async function handleBuyProduct() {
+        try {
+            setIsCreatingCheckoutSession(true)
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+            const { checkoutUrl } = response.data
+
+            window.location.href = checkoutUrl
+        } catch (err) {
+            // Conecetar com alguma ferramenta de observabilidade (Datadog / Sentry)
+            setIsCreatingCheckoutSession(false)
+            alert('Falha ao redirecionar ao checkout')
+        }
+    }
+
     const { isFallback } = useRouter()
 
     if (isFallback) {
@@ -32,7 +53,7 @@ export default function Product({ product }: ProductProps) {
                 <span>{product.price}</span>
 
                 <p>{product.description}</p>
-                <button>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDetails>
@@ -69,6 +90,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                     currency: 'BRL',
                 }).format(price.unit_amount as number / 100),
                 description: product.description,
+                defaultPriceId: price.id,
             }
         },
         revalidate: 60 * 60 * 1, // 1 hour
